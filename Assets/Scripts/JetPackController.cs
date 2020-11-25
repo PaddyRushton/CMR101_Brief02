1a0f5f2e15cc3a915c9d4e00b56177ac29f199b7
PaddyRushton <paddyrushton@gmail.com>
2020-11-25 21:41:31 +1100

    - Changed back to 'Input Manager (Old)' - Added MovementProvider script. - Added basic geometry to explore (open and enclosed spaces)

---
+++ Assets/Scripts/JetPackController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPackController : MonoBehaviour
+{
+    
+        public GameObject jetNozzle;
+
+
+        private void Update()
+        {
+            ThrottleOn();
+        }
+
+
+        public void ThrottleOn()
+        {
+            Vector3 jetDirection = jetNozzle.transform.up;
+            if (Input.GetAxis("Axis1D.PrimaryIndexTrigger") > 0.2f)
+            {
+                jetNozzle.GetComponent<Rigidbody>().AddForce(jetDirection*1000, ForceMode.VelocityChange);
+            }
+
+            else if (Input.GetAxis("Axis1D.PrimaryIndexTrigger") < 0.2f)
+            {
+                jetNozzle.GetComponent<Rigidbody>().AddForce(jetDirection * 0, ForceMode.VelocityChange);
+            }
+        }
+    
+}
+
